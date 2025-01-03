b1bfae9292b88323e5b5b3b6d82cfb52f1272819
Chris House <chhouse@microsoft.com>
2025-01-03 10:56:28 -0600

    added proj

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FrameworkWebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            Trace.WriteLine("Hello from Index action method");
+            Trace.WriteLine("More Tracing");
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            Trace.WriteLine("Hello from About action method");
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            Trace.WriteLine("Hello from Contact action method");
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
